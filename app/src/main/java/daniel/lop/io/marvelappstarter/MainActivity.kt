b7ee2f2256ba729ae1d84b4291572cad0be91ce5
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-04-22 19:19:29 -0300

    add fun initViews in MainActivity.kt

--- app/src/main/java/daniel/lop/io/marvelappstarter/MainActivity.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/MainActivity.kt
@@ -2,15 +2,33 @@ package daniel.lop.io.marvelappstarter
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
 import daniel.lop.io.marvelappstarter.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
+    private lateinit var navHostFragment: NavHostFragment
     private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        initViews(binding)
+    }
+
+    private fun initViews(binding: ActivityMainBinding) {
+        navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.mainFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        binding.mainBottomNavigation.apply {
+            setupWithNavController(navController)
+            setOnNavigationItemReselectedListener {
+
+            }
+        }
+
     }
 }
