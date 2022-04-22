ee096440e943e2d430b069f57884ab78abc4764f
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-04-22 17:44:51 -0300

    create BaseFragment

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/ui/base/BaseFragment.kt
@@ -0,0 +1,31 @@
+package daniel.lop.io.marvelappstarter.ui.base
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import androidx.lifecycle.ViewModel
+import androidx.viewbinding.ViewBinding
+
+abstract class BaseFragment<viewBinding: ViewBinding, viewModel: ViewModel>: Fragment() {
+    private var _binding: viewBinding? = null
+    protected  val binding get() = _binding!!
+    protected  abstract val viewModel: viewModel
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        _binding =  getViewBinding(inflater, container)
+        return binding.root
+    }
+
+    protected abstract fun getViewBinding(inflater: LayoutInflater, container: ViewGroup?): viewBinding
+
+    override fun onDestroyView() {
+        super.onDestroyView()
+        _binding = null
+    }
+}
