a295a6f20bd68eda6d8545ca69e90a418fcc065e
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-04-23 09:17:52 -0300

    create object Module

---
+++ app/src/main/java/daniel/lop/io/marvelappstarter/di/Module.kt
@@ -0,0 +1,41 @@
+package daniel.lop.io.marvelappstarter.di
+
+import dagger.Module
+import dagger.Provides
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import daniel.lop.io.marvelappstarter.data.remote.ServiceApi
+import daniel.lop.io.marvelappstarter.utils.Constants
+import okhttp3.OkHttpClient
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
+import javax.inject.Singleton
+
+
+@Module
+@InstallIn(SingletonComponent::class)
+object Module {
+
+    @Singleton
+    @Provides
+    fun provideOkHttpClient(): OkHttpClient {
+        return OkHttpClient()
+    }
+
+    @Singleton
+    @Provides
+    fun provideRetrofit(client: OkHttpClient): Retrofit {
+        return Retrofit.Builder()
+            .baseUrl(Constants.BASE_URL)
+            .addConverterFactory(
+                GsonConverterFactory.create()
+            )
+            .client(client).build()
+    }
+
+    @Singleton
+    @Provides
+    fun provideServiceApi(retrofit: Retrofit): ServiceApi {
+        return retrofit.create(ServiceApi::class.java)
+    }
+}
