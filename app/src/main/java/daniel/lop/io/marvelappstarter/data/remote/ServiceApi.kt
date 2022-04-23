db73d10d36bccffd5f3baf5ab36c340cf67e761e
rita monteiro <ritaalvesmonteiro@gmail.com>
2022-04-23 09:11:31 -0300

    add getCharacterList and getComic in ServiceApi.kt

--- app/src/main/java/daniel/lop/io/marvelappstarter/data/remote/ServiceApi.kt
+++ app/src/main/java/daniel/lop/io/marvelappstarter/data/remote/ServiceApi.kt
@@ -1,6 +1,10 @@
 package daniel.lop.io.marvelappstarter.data.remote
 
+import daniel.lop.io.marvelappstarter.data.model.character.CharacterResult
+import daniel.lop.io.marvelappstarter.data.model.comic.ComicResult
+import retrofit2.Response
 import retrofit2.http.GET
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 interface ServiceApi {
@@ -8,5 +12,11 @@ interface ServiceApi {
     @GET("characters")
     suspend fun getCharacterList(
         @Query("nameStartWith") nameStartWith: String? = null
-    )
+    ): Response<CharacterResult>
+
+    @GET("characters/{characterId}/comic")
+    suspend fun getComic(
+        @Path(value = "characterId", encoded = true) characterId: Int
+    ): Response<ComicResult>
+
 }
